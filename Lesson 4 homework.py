f62635435d3347f1ec07e15f422b1191918d62fe
andriiminets <andreiminets@gmail.com>
2023-02-25 14:45:43 +0200

    Lesson 4 homework: Тернарні оператори

---
+++ Lesson 4 homework.py
@@ -0,0 +1,34 @@
+# Task 1
+value = 100
+new_value = (value / 2) if value < 100 else (-value)
+
+print(new_value)
+
+# Task 2
+
+value = 500
+new_value = 1 if value < 100 else 0
+
+print(new_value)
+
+# Task 3
+
+value = 100
+new_value = True if value < 100 else False
+
+print(new_value)
+
+# Task 4
+
+my_str = "mystring"
+new_my_str = (my_str + my_str) if len(my_str) < 5 else my_str
+
+print(new_my_str)
+
+# Task 5
+
+my_str = "mystring"
+new_my_str = (my_str + my_str[::-1]) if len(my_str) < 5 else my_str
+
+print(new_my_str)
+

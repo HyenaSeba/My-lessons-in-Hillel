e13947187f42cba84e7b081b48b86cca2fba3282
andriiminets <andreiminets@gmail.com>
2023-03-02 19:12:04 +0200

    Lesson 6 homework: практичне завдання

---
+++ Lesson 6 homework.py
@@ -0,0 +1,26 @@
+# Task 1
+
+my_list = [1, 10, 213, -100, 2311, -293, 12, 934]
+for i in my_list:
+    if i > 100:
+        print(i)
+
+# Task 2
+
+my_list = [1, 20, 213, -102, 2312, -21312, 22, 1241]
+my_results = []
+for i in my_list:
+    if i > 100:
+        my_results.append(i)
+print(my_results)
+
+# Task 3
+
+my_list = [1, 20, 213, -102, 2312, -21312, 22, 1241]
+last_sum = my_list[-1] + my_list[-2]
+
+if len(my_list) < 2:
+    my_list.append(0)
+else:
+    my_list.append(last_sum)
+print(my_list)

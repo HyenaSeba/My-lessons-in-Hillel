0fd20a8670b3186119facd0a6987ebda5186e4a0
HyenaSeba <124877173+HyenaSeba@users.noreply.github.com>
2023-03-09 15:05:08 +0000

    Lesson 8 homework

---
+++ Lesson 8 homework.py
@@ -0,0 +1,10 @@
+# Task 1 
+
+my_list = ['str', 'asdas', 'write', '12asd213', 'ashda', '2131']
+new_list = []
+
+my_list_en = enumerate(my_list)
+
+for i in my_list_en:
+    new_list.append(i)
+    print(new_list)
